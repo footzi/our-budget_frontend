2fdd2a80b6fc5076151af13428d3209b5f740a14
Vladislav Manakov <vsmanakov@avito.ru>
2022-11-29 21:23:11 +0800

    Добавляет расширение utc

--- src/utils/formatToHumanDate/index.ts
+++ src/utils/formatToHumanDate/index.ts
@@ -4,4 +4,4 @@ import dayjs from 'dayjs';
  * Форматирует дату с бэка в дату которая используется на фронте
  *  2021-07-01 в 01.07.2021
  */
-export const formatToHumanDate = (date: string): string => dayjs(date).format('DD.MM.YYYY');
+export const formatToHumanDate = (date: string): string => dayjs.utc(date).format('DD.MM.YYYY');
