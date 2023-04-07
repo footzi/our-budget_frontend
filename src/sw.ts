d8740bf0b8e3492fd240220821390be73f3e08e6
Vladislav Manakov <vsmanakov@avito.ru>
2023-04-07 14:48:11 +0900

    Debug

--- src/sw.ts
+++ src/sw.ts
@@ -1,6 +1,7 @@
 export const initServiceWorker = () => {
   window.addEventListener('load', () => {
     if (navigator.serviceWorker) {
+      console.log(`${PUBLIC_PATH}service-worker.js`);
       navigator.serviceWorker.register(`${PUBLIC_PATH}service-worker.js`);
     }
   });
