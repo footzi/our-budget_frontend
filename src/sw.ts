ac1c73ac25f5cb7b23b46c73c00f44a0298bf554
Vladislav Manakov <vsmanakov@avito.ru>
2023-04-14 14:05:53 +0900

    Исправляет версту блоков

--- src/sw.ts
+++ src/sw.ts
@@ -1,7 +1,6 @@
 export const initServiceWorker = () => {
   window.addEventListener('load', () => {
     if (navigator.serviceWorker) {
-      console.log(`${PUBLIC_PATH}service-worker.js`);
       navigator.serviceWorker.register(`${PUBLIC_PATH}service-worker.js`);
     }
   });
